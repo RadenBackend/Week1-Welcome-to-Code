f8880fae2691d42be66664fa8791f17b374c75c9
RadenBackend <kevanganteng5@gmail.com>
2025-06-09 18:10:00 +0800

    add tugas3 part3

---
+++ quiz/part3/tugas3.js
@@ -0,0 +1,15 @@
+// Hint: kamu akan menggunakan kondisional juga di kasus ini.
+
+// Problem
+// Buatlah sebuah perulangan 1 - 100 dengan pertambahan counter sebanyak 1
+// Di dalam perulangan, periksa setiap angka counter:
+
+// Apabila angka counter adalah angka genap, tuliskan GENAP
+// Apabila angka counter adalah angka ganjil, tuliskan GANJIL
+
+for(let angka = 1; angka < 101; angka++)
+   if(angka % 2 === 0) {
+      console.log(`${angka} Bilangan Genap`)
+   } else {
+     console.log(`${angka} Bilangan Ganjil`)
+   }
