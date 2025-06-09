edc246b7738db99dafd26a013f7725c8a3a1f483
RadenBackend <kevanganteng5@gmail.com>
2025-06-09 17:05:13 +0800

    add tugas2 part3

---
+++ quiz/part3/tugas2.js
@@ -0,0 +1,14 @@
+// Problem
+// Pada tugas ini kamu diminta untuk melakukan looping dalam JavaScript dengan menggunakan syntax for. 
+// Untuk membuat tantangan ini lebih menarik, kamu juga diminta untuk membuat suatu looping yang menghitung maju dan 
+// menghitung mundur. Jangan lupa tampilkan di console juga judul 'LOOPING PERTAMA' dan 'LOOPING KEDUA'.
+//tampilkan output yang menunjukan kalau looping itu maju pada looping pertama dan mundur pada looping kedua
+
+for(let neko = 0; neko < 6; neko++) {
+    console.log('Looping Pertama : ', neko)
+} for(let Meow = 5; Meow > -1; Meow--) {
+    console.log('Looping Kedua : ', Meow)
+}
+
+// Meow :3    
+
